d8d17af057687de7863f0bb97e00512b7746bf5b
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-09 21:09:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-cy.get('.cube')
-  .first()
-  .trigger('mousedown', { which: 1, pageX: 100, pageY: 100, force: true })
-  .trigger('mousemove', { pageX: 200, pageY: 100, force: true })
-  .wait(200)
-  .trigger('mouseup', { force: true });
-
-cy.get('.cube').should(($el) => {
-  const left = parseInt($el[0].style.left);
-  expect(left).to.be.greaterThan(0);
-});
+// Your code here.
