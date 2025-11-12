874dd77565e5fecfaffe043e8ef90ae9bb1e4cdc
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-12 19:44:21 +0530

    created a submission

--- script.js
+++
@@ -1,4 +0,0 @@
-cy.get('.items')
-  .trigger('mousedown', { which: 1, pageX: 493, pageY: 391 })
-  .trigger('mousemove', { pageX: 271, pageY: 391 }) // larger delta = bigger scroll
-  .trigger('mouseup', { force: true });
